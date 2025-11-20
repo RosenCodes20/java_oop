89ef2e6d0464035471528f2ca6dade8877f0e530
Rosen Ivanov <rrirrirri08@gmail.com>
2025-11-20 17:38:25 +0200

    feat: added new class

---
+++ week05/src/Book.java
@@ -0,0 +1,13 @@
+public class Book {
+    public String title;
+    public String authorName;
+    public int pages;
+    public String genre;
+
+    public Book(String title, String authorName, int pages, String genre) {
+        this.title = title;
+        this.authorName = authorName;
+        this.pages = pages;
+        this.genre = genre;
+    }
+}
